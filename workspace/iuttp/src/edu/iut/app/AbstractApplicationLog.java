0cd8722e5f50c55fdcd3ac64ba9bf09534946b28
Julien Lacroix <julien.lacroix@u-psud.fr>
2015-11-25 10:17:58 +0100

    Modif workspace

--- workspace/iuttp/src/edu/iut/app/AbstractApplicationLog.java
+++ workspace/iuttp/src/edu/iut/app/AbstractApplicationLog.java
@@ -16,6 +16,18 @@ public abstract class AbstractApplicationLog implements IApplicationLog {
     /** TP1 : Fonction venant de l'interface par héritage */
 	public abstract void setMessage(String message);
 
+	public String getMessage() {
+		return message;
+	}
+
+	public void addListener(IApplicationLogListener listener) {
+		listeners.add(listener);
+	}
+	
+	public IApplicationLogListener[] getApplicationLogListeners() {
+		return (IApplicationLogListener[])listeners.toArray();
+	}
+
 	/** Listener action */
 	protected void fireMessage(String level, String message) {
 		for (IApplicationLogListener listener_i : listeners) {
